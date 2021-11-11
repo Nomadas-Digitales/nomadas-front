d6239fe5505fac012edf4ea7ba671d76f09f9cbc
maidersonn <maidersonn@gmail.com>
2021-11-11 18:21:45 +0100

    feat: hook with fetch to filterHoueses

---
+++ src/hooks/useFetchFilteredHouses.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { houses } from "services";
+
+const useFetchFilteredHouses = (filters) => {
+  const [filteredHouses, setFilteredHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await houses.getByFilters(
+        filters.distance,
+        filters.priceMin,
+        filters.priceMax,
+        filters.distanceBeach,
+        filters.internet
+      );
+      console.log("resultsOfFetch", results);
+      setFilteredHouses(results.data);
+    };
+    fetchData();
+  }, [filters]);
+  return filteredHouses;
+};
+
+export default useFetchFilteredHouses;
