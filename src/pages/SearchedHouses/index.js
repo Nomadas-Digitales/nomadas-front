303697076ac45a112493a15724e5d146c61c4a41
maidersonn <maidersonn@gmail.com>
2021-11-11 18:23:31 +0100

    feat: page searchedHouses

--- src/pages/SearchedHouses/index.js
+++ src/pages/SearchedHouses/index.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import useFetchFilteredHouses from "hooks/useFetchFilteredHouses";
+
 const SearchedHouse = () => {
-  return <h1>SearchedHouse</h1>;
+  const [filters, setFilters] = useState({
+    distance: "",
+    priceMin: "",
+    priceMax: "",
+    distanceBeach: "",
+    internet: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const houses = useFetchFilteredHouses(filters);
+  console.log("houses", houses);
+  return (
+    <div>
+      <h1>SearchedHouse</h1>
+    </div>
+  );
 };
 
 export default SearchedHouse;
