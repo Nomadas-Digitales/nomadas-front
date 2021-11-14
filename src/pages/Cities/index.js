8cbd6f7553e35553ad2271868e4a437695d42867
Protero <protero@outlook.es>
2021-11-14 10:51:30 +0100

    chore: added city elements and sytles

--- src/pages/Cities/index.js
+++ src/pages/Cities/index.js
@@ -1,15 +1,16 @@
 import "./cities.scss";
 import { Link } from "react-router-dom";
+import ComboBox from "../../Components/ComboBox";
 const Cities = () => {
   return (
     <section>
-      <section className="header">
+      <section className="headerCity">
         <section className="navContainer">
           <div className="searchContainer1">
+            <ComboBox className="searchInput"/>
             <input type="text" className="searchInput" placeholder="Buscar" />
           </div>
-          <p>¿Aún no te has decidido?</p>
-
+          <p>¿AÚN NO HAS DECIDIDO?</p>
           <Link className="link">
             <button className="housesButton1">
               ¡TE <br />
@@ -18,49 +19,45 @@ const Cities = () => {
           </Link>
         </section>
       </section>
-      <main>
-        <ul>
-          <li className="city">
-            <div>
-              <img
-                alt="foto"
-                src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218"
-              />
+
+      
+      <div class="contenedorPrincipal"> 
+        <div class="contenedorFila">
+            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="150"/>
+            <div class="contenedorTextos">
+                <p>Valencia</p>
+                <p>1.254 Viviendas</p>
+                <p>2184€/mes</p>
+                <div class="contenedorIconos">
+                    <p>un icono</p>
+                    <p>el otro</p>
+                </div>
             </div>
-            <div>
-              <h2>Valencia</h2>
-              <p>dsffsdfsdfsdfsdfsd</p>
-              <p>Valoración</p>
+        </div>
+        <div class="contenedorFila">
+            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="150"/>
+            <div class="contenedorTextos">
+                <p>Valencia</p>
+                <p>Características del piso</p>
+                <div class="contenedorIconos">
+                    <p>un icono</p>
+                    <p>el otro</p>
+                </div>
             </div>
-          </li>
-          <li className="city">
-            <div>
-              <img
-                alt="foto"
-                src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218"
-              />
+        </div>
+        <div class="contenedorFila">
+            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="150"/>
+            <div class="contenedorTextos">
+                <p>Valencia</p>
+                <p>Características del piso</p>
+                <div class="contenedorIconos">
+                    <p>un icono</p>
+                    <p>el otro</p>
+                </div>
             </div>
-            <div>
-              <h2>Valencia</h2>
-              <p>dsffsdfsdfsdfsdfsd</p>
-              <p>Valoración</p>
-            </div>
-          </li>
-          <li className="city">
-            <div>
-              <img
-                alt="foto"
-                src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218"
-              />
-            </div>
-            <div>
-              <h2>Valencia</h2>
-              <p>dsffsdfsdfsdfsdfsd</p>
-              <p>Valoración</p>
-            </div>
-          </li>
-        </ul>
-      </main>
+        </div>
+    </div>
+
     </section>
   );
 };
