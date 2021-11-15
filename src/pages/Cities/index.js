a8e73b929b5a73d09989ffbbf5e02615d4d3d31f
Protero <protero@outlook.es>
2021-11-15 19:22:27 +0100

    merge

--- src/pages/Cities/index.js
+++ src/pages/Cities/index.js
@@ -23,8 +23,9 @@ const Cities = () => {
       
       <div class="contenedorPrincipal"> 
         <div class="contenedorFila">
-            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="150"/>
+            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="170"/>
             <div class="contenedorTextos">
+            <Link to="/cities/valencia">
                 <p>Valencia</p>
                 <p>1.254 Viviendas</p>
                 <p>2184€/mes</p>
@@ -32,28 +33,35 @@ const Cities = () => {
                     <p>un icono</p>
                     <p>el otro</p>
                 </div>
+                </Link>
             </div>
         </div>
         <div class="contenedorFila">
-            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="150"/>
+            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="170"/>
             <div class="contenedorTextos">
+            <Link to="/cities/valencia">
                 <p>Valencia</p>
-                <p>Características del piso</p>
+                <p>1.254 Viviendas</p>
+                <p>2184€/mes</p>
                 <div class="contenedorIconos">
                     <p>un icono</p>
                     <p>el otro</p>
                 </div>
+                </Link>
             </div>
         </div>
         <div class="contenedorFila">
-            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="150"/>
+            <img src="https://images.adsttc.com/media/images/5ec7/e38f/b357/6579/0d00/00ce/newsletter/bionyl-1.jpg?1590158218" alt="foto" width="150" height="170"/>
             <div class="contenedorTextos">
+            
                 <p>Valencia</p>
-                <p>Características del piso</p>
+                <p>1.254 Viviendas</p>
+                <p>2184€/mes</p>
                 <div class="contenedorIconos">
                     <p>un icono</p>
                     <p>el otro</p>
                 </div>
+            
             </div>
         </div>
     </div>
