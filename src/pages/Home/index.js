2c24e2ab774eb6fa41514324a30af1949652202d
Protero <protero@outlook.es>
2021-11-24 19:09:13 +0100

    chore: deleted tag link

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,22 +7,12 @@ import fondo from "../../static/Fondo.png";
 const Home = () => {
   return (
     <section>
-      {/* <div class="imgFondo">
+       <div className="wrap">
         <section className="backGround">
           <img className="logo" src={logo} />
           <img className="textLogo" src={textLogo} />
           <button className="buttonGreen">Iniciar sesión</button>
           <button className="buttonWhite">Registraste</button>
-          <Link to="/cities">Continuar como invitado</Link>
-       </section>
-      </div> */}
-      <div className="wrap">
-        <section className="backGround">
-          <img className="logo" src={logo} />
-          <img className="textLogo" src={textLogo} />
-          <button className="buttonGreen">Iniciar sesión</button>
-          <button className="buttonWhite">Registraste</button>
-          <Link to="/cities">Continuar como invitado </Link>
           <a href="/cities">Continuar como invitado {'>'}</a>
         </section>
         <div class="imgFondo"><img src={fondo}/></div>
