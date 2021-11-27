1cd9c589ec251606b4a8b009baec60fc54a60075
Protero <protero@outlook.es>
2021-11-27 15:47:38 +0100

    Feat: Added InterFavorites

--- src/pages/InterFavorites/index.js
+++ src/pages/InterFavorites/index.js
@@ -1,5 +1,37 @@
+import "./interFavorites.scss";
+import imgFavoritos from "../../static/Fondo-Favoritos.jpg";
+
+import { Link, useHistory } from "react-router-dom";
+
+import Nav from "Components/Nav";
+
 const InterFavorites = () => {
-  return <h1>InterFavorites</h1>;
+  const history = useHistory();
+
+  return (
+    <section className="wrapper">
+      <Nav>
+        <a
+          aria-label="Volver a la página anterior"
+          className="icon-TipoFlechaAtras"
+          onClick={history.goBack}
+        ></a>
+        <span className="icon-TipoMenuHamb falseClick"></span>
+      </Nav>
+      <div className="contentFav">
+        <h1 className="header1">Lleva tus favoritos contigo</h1>
+        <Link to="/login" className="linkGreen">
+          Iniciar sesión
+        </Link>
+        <Link to="#" className="linkGreen linkWhite">
+          Registrate
+        </Link>
+      </div>
+      <div className="imgFondoFav">
+        <img src={imgFavoritos} alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default InterFavorites;
