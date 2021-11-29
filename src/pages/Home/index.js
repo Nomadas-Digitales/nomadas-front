f603ff97732608cde66fd5373842a721c9992963
maidersonn <maidersonn@gmail.com>
2021-11-29 16:15:16 +0100

    feat: navigate from home to login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,24 @@
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./home.scss";
 import logo from "../../static/LOGO/PNG/WorkAbroad-13.png";
 import textLogo from "../../static/LOGO/PNG/WorkAbroad-12.png";
-import fondo from "../../static/fondo-home.png";
 
 const Home = () => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/login");
+  };
   return (
     <section>
-       <div className="wrap">
+      <div className="wrap">
         <section className="backGround">
           <img className="logo" alt="Logotipo Nomadas digitales" src={logo} />
           <img className="textLogo" alt="Work Abroad" src={textLogo} />
-          <button className="buttonGreen">Iniciar sesión</button>
+          <button className="buttonGreen" onClick={handleClick}>
+            Iniciar sesión
+          </button>
           <button className="buttonWhite">Registraste</button>
-          <a href="/cities">Continuar como invitado {'>'}</a>
+          <a href="/cities">Continuar como invitado {">"}</a>
         </section>
       </div>
     </section>
